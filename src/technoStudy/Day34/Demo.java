c024e37939e330f96001a4d5d954464c3d66153d
Techno Study <56236382+technoStudy@users.noreply.github.com>
2020-11-07 06:45:43 +0600

    Update Demo.java

--- src/technoStudy/Day34/Demo.java
+++ src/technoStudy/Day34/Demo.java
@@ -3,5 +3,6 @@ package technoStudy.Day34;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        // this comment was added from GitHuB
     }
 }
