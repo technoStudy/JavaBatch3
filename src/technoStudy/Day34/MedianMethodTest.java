fe6d094cd5f9c6d09636b080099566df07cc07ef
daulet2030 <daulet2030@gmail.com>
2020-11-07 08:35:55 +0600

    renaming test class name

--- src/technoStudy/Day34/MedianMethodTest.java
+++ src/technoStudy/Day34/MedianMethodTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MeanMethodTest {
+public class MedianMethodTest {
     @Test
     public void testCase1(){
         List<Integer> input = new ArrayList<>(Arrays.asList(3, 5, 12));
