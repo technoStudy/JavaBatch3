1e569e0a8b56484b4ef629526ca29fc81d1fe480
daulet2030 <daulet2030@gmail.com>
2020-11-07 07:32:53 +0600

    added test cases 1 and 2

--- src/technoStudy/Day34/Demo.java
+++ src/technoStudy/Day34/Demo.java
@@ -6,10 +6,34 @@ import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
+        testCase1();
+        testCase2();
+    }
+
+    private static void testCase1() {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3));
+        ListStatistics list1Stats = new ListStatistics(list1);
+        Integer actual = list1Stats.getMax();
+        Integer expected = 3;
+
+        if(expected.equals(actual)){
+            System.out.println("Test case 1 is correct!");
+        } else {
+            System.out.println("Test case 1 is incorrect, expected " + expected + " but got " + actual);
+        }
+    }
+
+    private static void testCase2() {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(4,2,3,7));
+        Integer expected = 7;
 
         ListStatistics list1Stats = new ListStatistics(list1);
+        Integer actual = list1Stats.getMax();
 
-        list1Stats.printList();
+        if(expected.equals(actual)){
+            System.out.println("Test case 2 is correct!");
+        } else {
+            System.out.println("Test case 2 is incorrect, expected " + expected + " but got " + actual);
+        }
     }
 }
