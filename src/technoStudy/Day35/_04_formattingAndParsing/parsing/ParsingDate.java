985a09aaf7c0f14357e2ce7500f49666a2803d28
daulet2030 <daulet2030@gmail.com>
2020-11-10 08:53:06 +0600

    day35, ParsingDate fixed

--- src/technoStudy/Day35/_04_formattingAndParsing/parsing/ParsingDate.java
+++ src/technoStudy/Day35/_04_formattingAndParsing/parsing/ParsingDate.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class ParsingDate {
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M d Y");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd uuuu");
         String electionDate = "11 03 2020";
         LocalDate  election = LocalDate.parse(electionDate, formatter);
         String toString = election.toString();
