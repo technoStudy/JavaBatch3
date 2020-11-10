53b788187f589507892921d5f761edcb9f6a94b4
daulet2030 <daulet2030@gmail.com>
2020-11-10 08:35:28 +0600

    day35, ParsingDate, will fix later

--- src/technoStudy/Day35/_04_formattingAndParsing/parsing/ParsingDate.java
+++ src/technoStudy/Day35/_04_formattingAndParsing/parsing/ParsingDate.java
@@ -1,7 +1,15 @@
 package technoStudy.Day35._04_formattingAndParsing.parsing;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ParsingDate {
     public static void main(String[] args) {
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M d Y");
+        String electionDate = "11 03 2020";
+        LocalDate  election = LocalDate.parse(electionDate, formatter);
+        String toString = election.toString();
+        System.out.println(toString);
     }
 }
