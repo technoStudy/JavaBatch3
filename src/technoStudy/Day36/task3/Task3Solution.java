3735f3997f780a809a0a380569e36f2ec2ae21fa
daulet2030 <daulet2030@gmail.com>
2020-11-11 08:44:03 +0600

    day36, task3 exact solution

--- src/technoStudy/Day36/task3/Task3Solution.java
+++ src/technoStudy/Day36/task3/Task3Solution.java
@@ -4,6 +4,7 @@ import technoStudy.Day36.task2.Task2Solution;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Task3Solution {
@@ -25,21 +26,19 @@ public class Task3Solution {
             date2 = Task2Solution.convertToDate(userInput);
         } while (date2 == null);
 
-        Integer dayDifference = getDayDifference(date1, date2);
+        Long dayDifference = getDayDifference(date1, date2);
 
         System.out.println("The day difference is " + dayDifference);
     }
 
-    public static Integer getDayDifference(LocalDate date1, LocalDate date2) {
-        Period between = null;
-        if(date1.isAfter(date2)){
-            between = Period.between(date1, date2);
+    public static Long getDayDifference(LocalDate date1, LocalDate date2) {
+        if (date1.isBefore(date2)) {
+            return ChronoUnit.DAYS.between(date1, date2);
         } else {
-            between = Period.between(date2, date1);
+            return ChronoUnit.DAYS.between(date2, date1);
         }
-
-        int yearDays = between.getYears() * 356;
-        int monthDays = between.getMonths() * 30;
-        return yearDays + monthDays + between.getDays();
+//        int yearDays = between.getYears() * 356;
+//        int monthDays = between.getMonths() * 30;
+//        return yearDays + monthDays + between.getDays();
     }
 }
