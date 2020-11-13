c4821bba93693a52bbeaa1e9cd98ec6ab367fec5
daulet2030 <daulet2030@gmail.com>
2020-11-13 06:30:54 +0600

    day37, CheckedException must be handled

--- src/technoStudy/Day37/exceptionTypes/CheckedException.java
+++ src/technoStudy/Day37/exceptionTypes/CheckedException.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 public class CheckedException {
     public static void main(String[] args) {
         FileInputStream stream = null;
-
+//        stream = new FileInputStream("someFile.txt");
         try {
             stream = new FileInputStream("someFile.txt");
         } catch (Exception e){
