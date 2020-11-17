140c51c4828469e553fb0a6b49165f7da8337783
daulet2030 <daulet2030@gmail.com>
2020-11-17 08:33:15 +0600

    day39, slide 12

---
+++ src/technoStudy/Day39/Slide12.java
@@ -0,0 +1,23 @@
+package technoStudy.Day39;
+
+public class Slide12 {
+    int instanceVariable = 1;
+
+    public static void main(String[] args) {
+        Slide12 instance = new Slide12();
+        instance.method();
+        System.out.println(instance.instanceVariable)
+//        localVar;
+        if(true) {
+            int anotherLocalVariable = 1;
+            System.out.println(instance.instanceVariable)
+
+        }
+//        anotherLocalVariable;
+    }
+
+    public void method() {
+        int localVar = 1;
+        System.out.println(instanceVariable);
+    }
+}
