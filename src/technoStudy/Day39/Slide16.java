c6e18a0538ebe1e32cd054b926ebc982b821b3bb
daulet2030 <daulet2030@gmail.com>
2020-11-17 08:56:08 +0600

    day39, slide 16

---
+++ src/technoStudy/Day39/Slide16.java
@@ -0,0 +1,14 @@
+package technoStudy.Day39;
+
+import java.util.ArrayList;
+
+public class Slide16 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<ArrayList<Object>> twoDimensional = new ArrayList<>(); // row and columns
+        ArrayList<ArrayList<ArrayList<Integer>>> threeDimensional =  new ArrayList<>();
+        ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> fourDimentsionaList = new ArrayList<>();
+
+        int[][][][][][] multiDimensionalArray; // six dimensions
+    }
+}
