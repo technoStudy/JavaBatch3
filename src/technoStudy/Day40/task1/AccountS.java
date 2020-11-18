4bee8427c5fb48a1e85e9ed04cab7cc5cba91683
daulet2030 <daulet2030@gmail.com>
2020-11-18 07:26:30 +0600

    day40, changing be reference

--- src/technoStudy/Day40/task1/AccountS.java
+++ src/technoStudy/Day40/task1/AccountS.java
@@ -1,11 +1,14 @@
 package technoStudy.Day40.task1;
 
+import java.util.Arrays;
+
 public class AccountS {
     // instance variable
     // constructors
     // methods
     private String userId;
     private double balance;
+    private String[] tags;
 
     public AccountS(String userId) {
         this.userId = userId;
@@ -23,5 +26,17 @@ public class AccountS {
 
     public String getUserId() {
         return userId;
+    }
+
+    public String[] getTags() {
+        // if you want to make sure that tags cannot be changed by reference
+//        return tags; // instead of returning the direct reference
+        // make a copy and the return copy
+        String[] copy = Arrays.copyOf(tags, 1);
+        return copy;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
     }
 }
--- src/technoStudy/Day40/task1/Demo.java
+++ src/technoStudy/Day40/task1/Demo.java
@@ -1,5 +1,7 @@
 package technoStudy.Day40.task1;
 
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
         AccountS account1 =  new AccountS("1");
@@ -8,6 +10,22 @@ public class Demo {
         account2.setBalance(-100);
 
         System.out.println(account1.getUserId());
+        String userId = account1.getUserId();
+        userId = "changed";
+        System.out.println(account1.getUserId());
+
         System.out.println(account2.getUserId());
+
+        String[] array = {"tag1"};
+        account1.setTags(array);
+
+        System.out.println(Arrays.toString(account1.getTags()));
+        String[] tags = account1.getTags();
+        tags[0] = "changedTag";
+        System.out.println(Arrays.toString(account1.getTags()));
+
+        account1.setTags(tags); // proper way to change
+        System.out.println(Arrays.toString(account1.getTags()));
+
     }
 }
