3c46d1ae2b016768dc17281314f8aff650a7dbf1
daulet2030 <daulet2030@gmail.com>
2020-11-18 06:50:37 +0600

    day40, task1

---
+++ src/technoStudy/Day40/task1/Demo.java
@@ -0,0 +1,10 @@
+package technoStudy.Day40.task1;
+
+public class Demo {
+    public static void main(String[] args) {
+        AccountS account1 =  new AccountS("1");
+        account1.setBalance(100);
+        AccountS account2 =  new AccountS("2");
+        account2.setBalance(-100);
+    }
+}
