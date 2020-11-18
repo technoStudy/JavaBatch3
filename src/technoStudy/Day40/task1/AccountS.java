063aa494b66851ae8578ad6addc857dd20ab4005
daulet2030 <daulet2030@gmail.com>
2020-11-18 06:48:03 +0600

    day40, task1 solution

---
+++ src/technoStudy/Day40/task1/AccountS.java
@@ -0,0 +1,21 @@
+package technoStudy.Day40.task1;
+
+public class AccountS {
+    // instance variable
+    // constructors
+    // methods
+    private String userId;
+    private double balance;
+
+    public AccountS(String userId) {
+        this.userId = userId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
