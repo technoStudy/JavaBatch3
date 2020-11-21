b653cf01f04a734ca9723dfc76f0e02446bbc8ed
daulet2030 <daulet2030@gmail.com>
2020-11-21 06:30:26 +0600

    day42, best practice

--- src/technoStudy/Day42/hiding/Demo.java
+++ src/technoStudy/Day42/hiding/Demo.java
@@ -7,11 +7,14 @@ public class Demo {
         Parent parent = new Parent();
         System.out.println(parent.calculateAge(LocalDate.now()));
 
+
         Child child = new Child();
         System.out.println(child.calculateAge(LocalDate.now()));
 
         Parent parentThatIsAChildInside = new Child(); // polymorphism
         System.out.println(parentThatIsAChildInside.calculateAge(LocalDate.now())); // hiding
+
+        Parent.calculateAge(LocalDate.now()); // to avoid doubt above call static methods using class name
 
         System.out.println(parentThatIsAChildInside.instanceMethod()); // overriding
     }
