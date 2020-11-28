951f040f9fe9c0aec64e880662bfa5537028d88e
daulet2030 <daulet2030@gmail.com>
2020-11-28 07:30:02 +0600

    day45, task1 solution

--- src/technoStudy/Day45/task1/DemoCars.java
+++ src/technoStudy/Day45/task1/DemoCars.java
@@ -1,7 +1,25 @@
 package technoStudy.Day45.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DemoCars {
     public static void main(String[] args) {
+        List<Car> list = new ArrayList<>();
         Car tesla = new Tesla("S1", 2020);
+        Car prius = new ToyotaPrius("P23", 2015);
+        Car mercedes = new Mercedes("M5", 2018);
+
+        list.add(tesla);
+        list.add(prius);
+        list.add(mercedes);
+
+        for (Car car : list) {
+            System.out.print(car.getModel());
+            if(car instanceof Electric) {
+                System.out.print(" electric");
+            }
+            System.out.println();
+        }
     }
 }
