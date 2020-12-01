96b77d013170cca929cde2500c83acdafc9f6906
daulet2030 <daulet2030@gmail.com>
2020-12-01 07:53:31 +0600

    day46, game implementing attack

--- src/technoStudy/Day46/dake/game/entities/Good.java
+++ src/technoStudy/Day46/dake/game/entities/Good.java
@@ -2,11 +2,19 @@ package technoStudy.Day46.dake.game.entities;
 
 import technoStudy.Day46.dake.game.Entity;
 
+import java.util.Random;
+
 public abstract class Good extends Entity {
     public void fight(Entity another) {
-        if(another instanceof Good) {
+        if (another instanceof Good) {
             return;
         }
-        System.out.print(this.getClass().getSimpleName() +" is attacking: " + another.getClass().getSimpleName() + " ") ;
+        System.out.print(this.getClass().getSimpleName() + " is attacking: " + another.getClass().getSimpleName() + " ");
+        boolean bareHands = new Random().nextBoolean();
+        if (bareHands) {
+            another.takeDamage(this);
+        } else {
+            another.takeDamage(this.weapon);
+        }
     }
 }
