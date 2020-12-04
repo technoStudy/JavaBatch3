0258b0aba515491f051cde159aabd1adfc30a95a
daulet2030 <daulet2030@gmail.com>
2020-12-04 07:28:06 +0600

    day48, inheritance overview

---
+++ src/technoStudy/Day48/inheritance/SubClass.java
@@ -0,0 +1,9 @@
+package technoStudy.Day48.inheritance;
+
+public class SubClass extends SuperClass {
+    void method() {
+        System.out.println(getA());
+        System.out.println(b);
+    }
+
+}
---
+++ src/technoStudy/Day48/inheritance/SuperClass.java
@@ -0,0 +1,10 @@
+package technoStudy.Day48.inheritance;
+
+public class SuperClass {
+    private int a;
+    protected int b;
+
+    public int getA() {
+        return a;
+    }
+}
