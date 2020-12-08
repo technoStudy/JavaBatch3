5558cb9764289a3b139790426b409325e9c24103
TechnoStudy <apps@techno.study>
2020-12-08 07:48:21 +0600

    day50, OCA questions constructors

---
+++ src/technoStudy/Day50/BlackRhino.java
@@ -0,0 +1,24 @@
+package technoStudy.Day50;
+
+class Rhinoceros {
+
+    public Rhinoceros() { System.out.print("1"); }
+
+}
+
+public class BlackRhino extends Rhinoceros {
+
+    public BlackRhino(int age) { System.out.print("2"); }
+
+    public BlackRhino() {
+//        super();
+        this(5);
+
+        System.out.print("3");
+
+    }
+
+    public static void main(String[] args) {
+        new BlackRhino();
+    }
+}
