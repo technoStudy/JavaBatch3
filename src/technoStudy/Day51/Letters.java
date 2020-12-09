e6bbfef018f69bbfefc802639527f695c7a2e583
TechnoStudy <apps@techno.study>
2020-12-09 06:31:57 +0600

    day50, OCA questions initializer blocks

---
+++ src/technoStudy/Day51/Letters.java
@@ -0,0 +1,37 @@
+package technoStudy.Day51;
+
+public class Letters {
+    {
+        System.out.print("1");
+    }
+    static {
+        System.out.println("static block initializer 1");
+    }
+
+    public Letters() {
+        {{ System.out.print("4"); }}
+    }
+
+    {
+        System.out.print("2");
+    }
+
+    public static void main(String[] args) {
+        Letters a = new Letters();
+        {
+            System.out.print("5");
+        }
+
+        System.out.println();
+        Letters b = new Letters();
+
+    }
+
+    {
+        System.out.print("3");
+    }
+
+    static {
+        System.out.println("static block initializer 2");
+    }
+}
