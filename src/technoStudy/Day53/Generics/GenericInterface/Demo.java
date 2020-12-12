c9574984956d8b721bd3370ac56ce60723353dfb
TechnoStudy <apps@techno.study>
2020-12-12 06:38:50 +0600

    day53, Generics Interface

--- src/technoStudy/Day53/Generics/GenericInterface/Demo.java
+++ src/technoStudy/Day53/Generics/GenericInterface/Demo.java
@@ -6,5 +6,10 @@ public class Demo {
     public static void main(String[] args) {
         GenericInterface<String> lambda1 = (s) -> System.out.println(s.toUpperCase());
         GenericInterface<Square> lambda2 = (s) -> System.out.println(s.side);
+
+        GenericInterfaceWithTwoParameters<String, Square> lambda3 = (p1, p2) -> {
+            System.out.println(p1.toUpperCase());
+            System.out.println(p2.side);
+        };
     }
 }
---
+++ src/technoStudy/Day53/Generics/GenericInterface/GenericInterfaceWithTwoParameters.java
@@ -0,0 +1,5 @@
+package technoStudy.Day53.Generics.GenericInterface;
+
+public interface GenericInterfaceWithTwoParameters<T, R> {
+    void print(T parameter1, R parameter2);
+}
